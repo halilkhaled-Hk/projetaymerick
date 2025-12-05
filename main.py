4051e35fb760e9ca407ac22e5484ca4739b9b199
halilkhaled-Hk <halilkhaled24@gmail.com>
2025-12-05 16:18:40 +0100

    Config: Add pre-commit hooks for Black and Flake8

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI(
-    title="ProjetAPI - Gestion des Soumissions de Projets Étudiants",
-    version="0.1.0"
-)
+app = FastAPI(title="ProjetAPI ", version="0.1.0")
+
 
 @app.get("/")
 def read_root():
-    return {"message": "Bienvenue sur l'API de gestion des soumissions de projets."}
+    return {"message": "Bienvenue "}
+
 
 # Le reste des endpoints sera ajouté dans les phases suivantes.
